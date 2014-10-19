0d14df34bce3a694773c3cab21fc3e39617161c0
Christian Kohler <kohler.christian@gmail.com>
2014-10-19 18:50:30 +0200

    Hardcoded page transitions

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
-var gulp = require('gulp');
+var gulp      = require('gulp');
 var webserver = require('gulp-webserver');
-var concat = require('gulp-concat');
+var concat    = require('gulp-concat');
+var bump      = require('gulp-bump');
 
 var paths = {
   sourcefiles : [
@@ -29,5 +30,10 @@ gulp.task('watch', function(){
   gulp.watch(paths.sourcefiles, ['dist']);
 });
 
+gulp.task('minor', function(){
+  gulp.src('./bower.json')
+  .pipe(bump({type:'minor'}))
+  .pipe(gulp.dest('./'));
+});
 
 gulp.task('default', ['watch', 'webserver']);
